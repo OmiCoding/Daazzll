5cd2ba562af27557781337b241fefe6d433199c6
Omi <omi@pop-os.localdomain>
2022-04-15 04:00:17 -0400

    Added some css to the absolute header

--- client/components/absHeader/HamburgerList.tsx
+++ client/components/absHeader/HamburgerList.tsx
@@ -16,25 +16,33 @@ const HamburgerList = React.forwardRef<HTMLDivElement, Props>(
             : "hamburger-nav-wrapper"
         }
       >
+        <div className="sign-log-wrapper">
+          <button className=""></button>
+          <button className=""></button>
+        </div>
         <nav className="hamburger-nav">
           <ul className="hamburger-nav-list">
             <li className="hb-nav__list__item">
+              <div className="hb-nav__item__svg-wrapper"></div>
               <Link to="/" className="hb-nav__item__link">
                 Home
               </Link>
             </li>
             <li className="hb-nav__list__item">
-              <Link to="/feed" className="hb-nav__list__link">
+            <div className="hb-nav__item__svg-wrapper"></div>
+              <Link to="/feed" className="hb-nav__item__link">
                 Feed
               </Link>
             </li>
             <li className="hb-nav__list__item">
-              <Link to="/market" className="hb-nav__list__link">
+            <div className="hb-nav__item__svg-wrapper"></div>
+              <Link to="/market" className="hb-nav__item__link">
                 Market
               </Link>
             </li>
             <li className="hb-nav__list__item">
-              <Link to="/contact" className="hb-nav__list__link">
+            <div className="hb-nav__item__svg-wrapper"></div>
+              <Link to="/contact" className="hb-nav__item__link">
                 Contact
               </Link>
             </li>
