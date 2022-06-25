66bf9ee135758c9b57e24f7c58332fc05c1bef4b
Omi <digicoder@protonmail.com>
2022-06-25 18:00:35 -0400

    Made some changes to the checkToken middleware, and made additions to the profile view

--- client/components/profile/ProfileContext.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement } from "react";
-
-interface ProviderProps {
-  children: ReactElement<any, any>;
-}
-
-const ProfileContext: React.FC<ProviderProps> = function ({ children }) {
-  return children;
-};
