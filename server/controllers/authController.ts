b2fce6c4100a330173412f6118262e43b7912721
Omi Omi <digicoder@protonmail.com>
2022-10-27 23:51:12 -0400

    Removed createLink() from authController file

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -313,34 +313,6 @@ export const checkAuth: RequestHandler = async function (
   }
 };
 
-export const createLink: RequestHandler = async function (req, res, next) {
-  try {
-    const { name, link } = req.body;
-
-    const result = await prismaClient.accounts.update({
-      where: {
-        email_username: {
-          email: "",
-          username: "",
-        },
-      },
-      data: {
-        profile: {
-          [name]: link,
-        },
-      },
-    });
-    
-    return res.status(200).json({
-      msg: "Link has been added to account!",
-      name,
-      link,
-    });
-  } catch (e: any) {
-    return next(e);
-  }
-};
-
 export const passGuest = function (
   req: Request,
   res: Response,
